48e6169e4b2f2faec0c5a21cc33d36742a94db5b
Natalia Tikhomirova <ntikhomirova@gmail.com>
2025-02-18 23:43:12 +0100

    first commit

--- src/main/java/herokuapp/pages/LoginPage.java
+++ src/main/java/herokuapp/pages/LoginPage.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage extends BasePage {
-
     public LoginPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
     }
